342bd3288692a075c5c0cb3de3db06b81c714fc6
vinu <85886469+vinu94-s@users.noreply.github.com>
2021-12-09 13:03:59 +0530

    Add files via upload

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -1,1 +1,47 @@
+import React, { Component, Fragment } from "react";
+import "./gallery.css";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import {allPostsExceptCurrentUsers} from "../../actions/postActions";
+import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner'
+import GalleryItem from './GalleryItem';
 
+
+class Gallery extends Component {
+  componentDidMount() {
+    this.props.allPostsExceptCurrentUsers();
+  }
+  render() {
+
+    const { posts, loadingPost } = this.props.post;
+    let imagesArray;
+    if(posts.length > 0) {
+    imagesArray = posts.map((post) => post.image);
+    }
+ 
+    return (
+      <div className='container masonry'>
+        {!loadingPost ? (
+          <Fragment>
+            {posts.map((post) => (
+              <GalleryItem key={post._id} image={post.image} id={post._id} />
+            ))}
+          </Fragment>
+        ) : (<div style={{ margin: "180px"}}>
+          <Spinner /></div>
+        )}
+      </div>
+    );
+  }
+} 
+Gallery.propTypes = {
+  allPostsExceptCurrentUsers: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+const mapStateToProps = state => ({
+  post: state.post
+})
+export default connect(mapStateToProps, { allPostsExceptCurrentUsers })(
+  Gallery
+);
