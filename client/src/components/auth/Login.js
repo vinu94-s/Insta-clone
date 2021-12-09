ead51ea541f14036e2ffeb30461991b4cffc46d5
vinu <85886469+vinu94-s@users.noreply.github.com>
2021-12-09 12:47:59 +0530

    Add files via upload

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,1 +1,139 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
+import mobile from "../../img/mobile.png";
+import axios from 'axios'
+import classnames from 'classnames';
+import "./login.css"; 
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import { getCurrentProfile } from "../../actions/profileActions";
+import PropTypes from 'prop-types';
 
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    this.props.loginUser(user);
+    // this.props.getCurrentProfile();
+  }
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/home');
+    }
+  }
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.auth.isAuthenticated) {
+        this.props.history.push('/home');
+      }
+      if (nextProps.errors) {
+        this.setState({ errors: nextProps.errors });
+    }
+  }
+    render() {
+    const { errors } = this.state;
+    const { email, password } = this.state;
+    const enabled = email.length > 0 && password.length > 0;
+    return (
+      <div className='margin'>
+        <img className='mobile d-none d-xl-block d-md-block' src={mobile} />
+        <div className='d-flex flex-column height'>
+          <div className='card'>
+            <div className='card-body'>
+              <img className='logo' src={logo} alt='instagram' />
+              <br />
+              <br />
+              <form onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='email'
+                    id='myText'
+                    className={classnames("form-control", {
+                      "is-invalid": errors.email,
+                    })}
+                    placeholder='Email'
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className='invalid-feedback'>{errors.email}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className={classnames("form-control ", {
+                      "is-invalid": errors.password,
+                    })}
+                    placeholder='Password'
+                    name='password'
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className='invalid-feedback'>{errors.password}</div>
+                  )}
+                </div>
+
+                <input
+                  type='submit'
+                  value='Log In'
+                  disabled={!enabled}
+                  // style={{
+                  //   width: "265px",
+                  //   height: "30px",
+                  //   marginTop: "10px",
+                  //   border: "None",
+                  // }}
+                  className='authButton'
+                />
+              </form>
+              <br />
+              <div>
+                <hr id='one' />
+                <span id='or'>OR</span>
+                <hr id='two' />
+              </div>
+              <br />
+              <p className='more-info'>
+                Don't have an account? &nbsp;
+                <span>
+                  <Link to='/signup'>Sign up</Link>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+Login.propTypes = {
+ getCurrentProfile: PropTypes.func.isRequired,
+ loginUser: PropTypes.func.isRequired,
+}
+const mapStateToProps = (state) => ({
+  errors: state.errors,
+  auth: state.auth,
+  
+});
+
+export default connect(mapStateToProps, { loginUser, getCurrentProfile })(Login);
---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,173 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import logo from '../../img/logo.png';
+import { connect } from 'react-redux';
+import { signupUser } from '../../actions/authActions';
+import { withRouter } from 'react-router-dom';
+import classnames from 'classnames';
+import "./signup.css"; 
+
+class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",      
+      password: "",
+      password2: "",
+      errors: {},
+      handle: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,      
+      password: this.state.password,
+      password2: this.state.password2,
+      handle: this.state.handle,
+    };
+
+    this.props.signupUser(newUser, this.props.history);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  render() {
+    const {errors} = this.state;
+    const {name} = this.state;
+    const enabled = name.length > 0 ;
+    return (
+      <div className='margin'>
+        <div className='d-flex flex-column'>
+          <div className='card'>
+            <div className='card-body'>
+              <img className='logo' src={logo} alt='instagram' />
+              <p className='info'>
+                Sign up to see photos and videos from your friends.
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.name,
+                    })}
+                    placeholder='Full Name'
+                    name='name'
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                  {errors.name && (
+                    <div className='invalid-feedback'>{errors.name}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.handle,
+                    })}
+                    placeholder='Username'
+                    name='handle'
+                    value={this.state.handle}
+                    onChange={this.onChange}
+                  />
+                  {errors.handle && (
+                    <div className='invalid-feedback'>{errors.handle}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='email'
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email,
+                    })}
+                    placeholder='Email'
+                    name='email'
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className='invalid-feedback'>{errors.email}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password,
+                    })}
+                    placeholder='Password'
+                    name='password'
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className='invalid-feedback'>{errors.password}</div>
+                  )}
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password2,
+                    })}
+                    placeholder='Password2'
+                    name='password2'
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                  />
+                  {errors.password2 && (
+                    <div className='invalid-feedback'>{errors.password2}</div>
+                  )}
+                </div>
+                <input
+                  type='submit'
+                  value='Sign up'
+                  disabled={!enabled}
+                  // style={{
+                  //   width: "265px",
+                  //   height: "30px",
+                  //   marginTop: "10px",
+                  //   border: "None",
+
+                  // }}
+                  className='authButton'
+                />
+              </form>
+              <br />
+              <p className='terms'>
+                By signing up,you agree to our{" "}
+                <b>Terms, Data Policy and Cookies Policy.</b>
+              </p>
+
+              <p className='more-info'>
+                Have an Account? &nbsp;
+                <span>
+                  <Link to='/'>Log in</Link>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { signupUser })(withRouter(Signup));
