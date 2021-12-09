05e3229124b855869876c3205383d88eadb5c15b
vinu <85886469+vinu94-s@users.noreply.github.com>
2021-12-09 13:20:46 +0530

    Add files via upload

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,9 @@
-
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
