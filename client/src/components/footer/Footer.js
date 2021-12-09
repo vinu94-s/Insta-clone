dc448abe789a514bc431626be8c420cab889a7d2
vinu <85886469+vinu94-s@users.noreply.github.com>
2021-12-09 13:02:30 +0530

    Add files via upload

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,1 +1,18 @@
+import React from "react";
+import "./footer.css";
 
+function Footer() {
+  return (
+    <div class="stickyfooter">
+      <footer>
+        <div id='f'>
+          ABOUT HELP PRESS API JOBS PRIVACY TERMS LOCATIONS TOP ACCOUNTS
+          HASHTAGS LANGUAGE
+        </div>
+        &copy; {new Date().getFullYear()} INSTAGRAM FROM FACEBOOK
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
